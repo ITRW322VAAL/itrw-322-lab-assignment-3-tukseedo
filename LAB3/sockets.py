12870576a147ee3a4798fb9f44958e6a2d769e1b
tukseedo <tukseedo@Godemode.com>
2019-10-05 13:06:28 +0200

    29854369 - Python socket created

---
+++ LAB3/sockets.py
@@ -0,0 +1,12 @@
+import socket
+import sys
+
+# Create socket using the following
+
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
